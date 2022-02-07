582a6413df13edc1aa24aca6a7614be0fe7fae9d
saimeerasingh <singhsaimeera@gmail.com>
2022-02-07 14:25:16 +0000

    add, subtract, multiply methods added

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,23 @@
+public class Calculator {
+    private int a;
+    private int b;
+
+    public Calculator(int a,int b){
+        this.a = a;
+        this.b = b;
+    }
+
+    public int addNumbers(int a, int b){
+        return a + b;
+    }
+
+    public int subtractNumbers(int a, int b){
+        return a - b;
+    }
+
+    public int multiplyNumbers(int a, int b){
+        return a * b;
+    }
+
+    
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,31 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+    Calculator calculator;
+
+    @Before
+    public void before(){
+        calculator = new Calculator(5,25);
+    }
+
+    @Test
+    public void canAddNumbers(){
+        assertEquals(30, calculator.addNumbers(5,25));
+    }
+
+    @Test
+    public void canSubtractNumbers(){
+        assertEquals(20,calculator.subtractNumbers(25,5));
+    }
+
+    @Test
+    public void canMutiplyNumbers(){
+        assertEquals(125,calculator.multiplyNumbers(5,25));
+    }
+
+
+
+}
